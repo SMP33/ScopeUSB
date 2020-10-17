4e4dc3866271ddd4bafb4906d82064827a83e109
Mikhail Slv <mikhail.slv@yandex.ru>
2020-10-17 17:39:50 +0300

    PA5 ADC + USB output

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -230,7 +230,7 @@ static void MX_ADC1_Init(void)
   }
   /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
   */
-  sConfig.Channel = ADC_CHANNEL_1;
+  sConfig.Channel = ADC_CHANNEL_5;
   sConfig.Rank = 1;
   sConfig.SamplingTime = ADC_SAMPLETIME_144CYCLES;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
